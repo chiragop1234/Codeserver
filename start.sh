b813b6a90514d3ffcf166208d1b23a4be66f7068
Chirag sadhwani <138006300+chiragop1234@users.noreply.github.com>
2025-05-11 14:46:44 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # Start code-server in the background
 code-server --port $PORT --disable-telemetry --auth none &
-# Wait briefly for code-server to start
+CODE_SERVER_PID=$!
+# Wait for code-server to start
 sleep 5
-# Start cloudflared to tunnel the code-server service
+# Check if code-server is running
+if ! kill -0 $CODE_SERVER_PID; then
+  echo "code-server failed to start"
+  exit 1
+fi
+# Start cloudflared to tunnel the code server
 ./cloudflared tunnel --url http://127.0.0.1:$PORT --metrics localhost:45678
