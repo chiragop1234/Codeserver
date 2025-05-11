2c2d0a0afb30480d34965d0c5537a6d656fd7814
Chirag sadhwani <138006300+chiragop1234@users.noreply.github.com>
2025-05-11 14:55:46 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,18 @@
 # Start code-server in the background
 code-server --port $PORT --disable-telemetry --auth none &
 CODE_SERVER_PID=$!
-# Wait for code-server to start
-sleep 5
+# Wait for code-server to start (up to 30 seconds)
+for i in {1..30}; do
+  if nc -z 127.0.0.1 $PORT; then
+    echo "code-server is listening on port $PORT"
+    break
+  else
+    echo "Waiting for code-server to start..."
+    sleep 1
+  fi
+done
 # Check if code-server is running
-if ! kill -0 $CODE_SERVER_PID; then
+if ! nc -z 127.0.0.1 $PORT; then
   echo "code-server failed to start"
   exit 1
 fi
